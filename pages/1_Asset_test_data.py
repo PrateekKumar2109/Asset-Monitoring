c47f028bf79d37a679e5800c971e4bf50911009b
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-07-05 02:13:18 +0530

    Update 1_Asset_test_data.py

--- pages/1_Asset_test_data.py
+++ pages/1_Asset_test_data.py
@@ -7,8 +7,7 @@ data_url="https://raw.githubusercontent.com/PrateekKumar2109/Asset-Monitoring/ma
 # Load dataframe here
 df_final = pd.read_csv(data_url)  # replace 'your_data.csv' with your dataframe file path
 
-
-def map_plot(df, texts, color):
+def map_plot(df, texts, color, font_size):
     fig, ax = plt.subplots(figsize=(15, 15))
 
     ax.scatter(df['Longitude'], df['Latitude'], color='blue')
@@ -17,7 +16,7 @@ def map_plot(df, texts, color):
         row = df.iloc[i]
         if texts:
             text = f"{row['Platform']}\n" + "\n".join([f"{text}: {row[text]}" for text in texts])
-            ax.text(row['Longitude'], row['Latitude'], text, va='bottom', ha='left', fontsize=8, color=color,
+            ax.text(row['Longitude'], row['Latitude'], text, va='bottom', ha='left', fontsize=font_size, color=color,
                     bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.2'))
         else:
             ax.text(row['Longitude'], row['Latitude'], f"{row['Platform']}", va='bottom', ha='left', fontsize=16,
@@ -35,19 +34,24 @@ option = st.sidebar.selectbox('Select Option', ('None', 'OP', 'GP', 'WI'), index
 if option == 'None':
     texts = []
     color = 'black'
+    font_size = 16
 elif option == 'OP':
     texts = ['LIQUID RATE(BLPD)', 'OIL(BOPD)', 'WATER(BWPD)', 'GAS LIFT RATE(M3/DAY)']
     color = 'black'
+    font_size = 8
 elif option == 'GP':
     df_final = df_final[df_final['Free gas'] > 0.1]
     texts = ['Free gas']
     color = 'green'
+    font_size = 14
 elif option == 'WI':
     df_final = df_final[df_final['INJECTION RATE(M3/DAY)'] > 0.1]
     texts = ['INJECTION RATE(M3/DAY)']
     color = 'blue'
+    font_size = 14
 
-fig = map_plot(df_final, texts, color)
+fig = map_plot(df_final, texts, color, font_size)
 st.pyplot(fig)
+
 
 
