315e2b51cdea73329b65fe81552594ab037ee8a8
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-07-05 01:13:23 +0530

    Update 1_Field Performance.py

--- pages/1_Field Performance.py
+++ pages/1_Field Performance.py
@@ -2,9 +2,9 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
-
+data_url=
 # Load dataframe here
-df_final = pd.read_csv('your_data.csv')  # replace 'your_data.csv' with your dataframe file path
+df_final = pd.read_csv(data_url)  # replace 'your_data.csv' with your dataframe file path
 
 def map_plot(df, texts):
     fig, ax = plt.subplots(figsize=(15, 15), subplot_kw=dict(projection=ccrs.PlateCarree()))
