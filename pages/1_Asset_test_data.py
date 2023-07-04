41b873c2cb300557d54348eed964c72333670c6b
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-07-05 01:36:04 +0530

    Update 1_Asset_test_data.py

--- pages/1_Asset_test_data.py
+++ pages/1_Asset_test_data.py
@@ -1,30 +1,28 @@
-import cartopy
+#import cartopy
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-import cartopy.crs as ccrs
+#import cartopy.crs as ccrs
 data_url="https://raw.githubusercontent.com/PrateekKumar2109/Asset-Monitoring/main/df_final.csv"
 # Load dataframe here
 df_final = pd.read_csv(data_url)  # replace 'your_data.csv' with your dataframe file path
 
+
 def map_plot(df, texts):
-    fig, ax = plt.subplots(figsize=(15, 15), subplot_kw=dict(projection=ccrs.PlateCarree()))
+    fig, ax = plt.subplots(figsize=(15, 15))
 
-    ax.set_extent([df['Longitude'].min()-1, df['Longitude'].max()+1, 
-                   df['Latitude'].min()-1, df['Latitude'].max()+1])
-
-    ax.coastlines(resolution='50m')
-    ax.gridlines(draw_labels=True)
+    ax.scatter(df['Longitude'], df['Latitude'], color='blue')
 
     for i in range(len(df)):
         row = df.iloc[i]
-        ax.scatter(row['Longitude'], row['Latitude'], color='blue')
         ax.text(row['Longitude'], row['Latitude'], 
                 "\n".join([f"{text}: {row[text]}" for text in texts]),
                 va='bottom', ha='left', fontsize=8,
                 bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.2'))
 
     plt.title('Platform Data')
+    plt.xlabel('Longitude')
+    plt.ylabel('Latitude')
     return fig
 
 st.title('NH Asset Platforms')
@@ -40,3 +38,4 @@ elif option == 'WI':
 
 fig = map_plot(df_final, texts)
 st.pyplot(fig)
+
