9cd2abf686891a3135fd6e610d60b9978c8a50b8
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-07-05 01:15:48 +0530

    Update and rename 1_Field Performance.py to 1_Asset_test_data.py

--- pages/1_Asset_test_data.py
+++ pages/1_Asset_test_data.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
-data_url=
+data_url="https://raw.githubusercontent.com/PrateekKumar2109/Asset-Monitoring/main/df_final.csv"
 # Load dataframe here
 df_final = pd.read_csv(data_url)  # replace 'your_data.csv' with your dataframe file path
 
