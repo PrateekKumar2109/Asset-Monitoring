9d0dbc76569b0ac48e53ad321efa9516380618aa
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-07-05 22:46:34 +0530

    Update 1_Asset_test_data.py

--- pages/1_Asset_test_data.py
+++ pages/1_Asset_test_data.py
@@ -1,39 +1,50 @@
-
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 data_url="https://raw.githubusercontent.com/PrateekKumar2109/Asset-Monitoring/main/df_final2.csv"
 # Load dataframe here
-df_final = pd.read_csv(data_url)  # replace 'your_data.csv' with your dataframe file path
-# Setting 'Free gas' value to 0.0 where 'Platform type' is either 'Process Complex' or 'Flare'
+df_final = pd.read_csv(data_url)  
 df_final.loc[df_final['Platform type'].isin(['Process Complex', 'Flare']), 'Free gas'] = 0.0
 
-st.set_page_config(layout="wide") # Make the layout wide
+st.set_page_config(layout="wide") 
 
 def map_plot(df, texts, color, font_size):
-    fig, ax = plt.subplots(figsize=(20, 16),dpi=600)  # Increase the size of the plot
-    ax.set_facecolor('#e6f3ff') # set light background in the plot
-    ax.scatter(df['Longitude'], df['Latitude'], color='blue', s=60)  # Increase the size of the points with s
-
+    fig, ax = plt.subplots(figsize=(20, 16),dpi=600)  
+    ax.set_facecolor('#e6f3ff') 
+    ax.scatter(df['Longitude'], df['Latitude'], color='blue', s=60) 
 
     for i in range(len(df)):
         row = df.iloc[i]
         if texts:
             text = f"{row['Platform']}\n" + "\n".join([f"{text}: {row[text]}" for text in texts])
-            ax.text(row['Longitude'], row['Latitude'], text, va='bottom', ha='left', fontsize=font_size, color=color,
+            if option == 'None' and row['Rig'] == 'yes':
+                text_color = 'orange'
+            else:
+                text_color = color
+            ax.text(row['Longitude'], row['Latitude'], text, va='bottom', ha='left', fontsize=font_size, color=text_color,
                     bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.2'))
         else:
             ax.text(row['Longitude'], row['Latitude'], f"{row['Platform']}", va='bottom', ha='left', fontsize=16,
                     color=color)
+    # Add a dashed line
+    start_lat = 18.710
+    start_lon = 72.315
+    angle = np.deg2rad(30)  # Convert the angle to radians
+    # Length of the line
+    length = 0.1  # Change this value to adjust the length of the line
+    end_lat = start_lat + length * np.sin(angle)
+    end_lon = start_lon + length * np.cos(angle)
+    plt.plot([start_lon, end_lon], [start_lat, end_lat], linestyle='dashed', color='black')
             
-    plt.title(' Offshore Platforms', fontsize=24)  # Increase the size of the title
-    plt.gca().axes.get_xaxis().set_visible(False)  # Hide x axis
-    plt.gca().axes.get_yaxis().set_visible(False)  # Hide y axis
+    plt.title(' Offshore Platforms', fontsize=24)  
+    plt.gca().axes.get_xaxis().set_visible(False)  
+    plt.gca().axes.get_yaxis().set_visible(False) 
 
     return fig
 
-st.title('NH Asset Platforms', )
+st.title('NH Asset Platforms')
 
 option = st.sidebar.selectbox('Select Option', ('None', 'OP', 'GP', 'WI', 'GI'), index=0)
 
@@ -62,7 +73,5 @@ elif option == 'GI':
     color = 'red'
     font_size = 11
 
-fig = map_plot(df_final, texts, color, font_size)
-st.pyplot(fig)
-
-
+# Create the map plot and add it to the Streamlit app
+st.pyplot(map_plot(df_final, texts, color, font_size))
