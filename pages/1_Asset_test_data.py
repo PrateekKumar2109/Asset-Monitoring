d79e569297dac0c4f12280303357d66d950b39bb
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-07-23 17:54:52 +0530

    Update 1_Asset_test_data.py

--- pages/1_Asset_test_data.py
+++ pages/1_Asset_test_data.py
@@ -49,17 +49,14 @@ text_res = {
     'R-7': {"Pi B: 2000-2100\\psi", "Pr B: 1100-1400\\psi","Pb B=1600\\psi","Pi M: 2000-2100\\psi", "Pr M: 1100-1400\\psi","Pb M=1400\\psi"},
     'R-9': {"Pi B: 2000-2100\\psi", "Pr B: 1100-1400\\psi","Pb B=1600\\psi","Pi M: 2000-2100\\psi", "Pr M: 1100-1400\\psi","Pb M=1400\\psi"}
 }
+
 new_text_box = {
     'text': "Aban-VIII: HE\nFD-IX: HN\nGD Chaaru: NLM5\nGD Chaaya: HS\nJT Angel: HT",
     'coords': {'lat': 18.488, 'long': 72.316}
 }
 
-
 for key, values in text_res.items():
     text_res[key] = {value.replace('\\psi', '') for value in values}
-
-#print(text_res)
-
 
 st.set_page_config(layout="wide") 
 
@@ -68,22 +65,6 @@ def map_plot(df, df_reserves, texts, font_size, show_lines=False):
     ax.set_facecolor('#e6f3ff') 
 
     sns.scatterplot(data=df, x='Longitude', y='Latitude', hue='Field', ax=ax, s=60)
-    
-    selected_columns = ['Oil Inplace', 'Oil Ultimate', 'Oil Production', 'Oil Balance Reserves']
-
-    if 'area' not in df_reserves.columns:
-        raise ValueError("'area' column not found in df_reserves DataFrame")
-    for area in df_reserves['area'].unique():
-        if area in area_coords:
-            df_area = df_reserves[df_reserves['area'] == area]
-            text_rows = []
-            for column in selected_columns:
-                value = df_area[column].sum()  
-                value = round(value, 2)  
-                text_rows.append(f"{column}: {value}")
-            full_text = f"{area}\n" + "\n".join(text_rows)
-            plt.text(area_coords[area]['long'], area_coords[area]['lat'], full_text, va='bottom', ha='left', fontsize=16,
-                     color='blue', bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.2'))
 
     for i in range(len(df)):
         row = df.iloc[i]
@@ -110,13 +91,6 @@ def map_plot(df, df_reserves, texts, font_size, show_lines=False):
             end_lon = line["start_lon"] + line["length"] * np.cos(angle_rad)
             plt.plot([line["start_lon"], end_lon], [line["start_lat"], end_lat], linestyle='dashed', color='black')
 
-    for area, texts in text_res.items():
-        full_text = f"{area}\n" + "\n".join(texts)
-        plt.text(text_res_coords[area]['long'], text_res_coords[area]['lat'], full_text, va='bottom', ha='left', fontsize=16,
-                 color='red', bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.2'))
-    if option == 'None':
-        plt.text(new_text_box['coords']['long'], new_text_box['coords']['lat'], new_text_box['text'], va='bottom', ha='left', fontsize=16, color='green', bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.2'))
-  
     plt.title('Offshore Platforms', fontsize=24)  
     plt.gca().axes.get_xaxis().set_visible(False)  
     plt.gca().axes.get_yaxis().set_visible(False) 
@@ -132,6 +106,26 @@ if option == 'None':
     texts = []
     font_size = 16
     show_lines = True
+
+    for area, texts in text_res.items():
+        full_text = f"{area}\n" + "\n".join(texts)
+        plt.text(text_res_coords[area]['long'], text_res_coords[area]['lat'], full_text, va='bottom', ha='left', fontsize=16,
+                 color='red', bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.2'))
+
+    plt.text(new_text_box['coords']['long'], new_text_box['coords']['lat'], new_text_box['text'], va='bottom', ha='left', fontsize=16, color='green', bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.2'))
+
+    for area in df_reserves['area'].unique():
+        if area in area_coords:
+            df_area = df_reserves[df_reserves['area'] == area]
+            text_rows = []
+            for column in selected_columns:
+                value = df_area[column].sum()  
+                value = round(value, 2)  
+                text_rows.append(f"{column}: {value}")
+            full_text = f"{area}\n" + "\n".join(text_rows)
+            plt.text(area_coords[area]['long'], area_coords[area]['lat'], full_text, va='bottom', ha='left', fontsize=16,
+                     color='blue', bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.2'))
+
 elif option == 'OP':
     df_final = df_final[df_final['LIQUID RATE(BLPD)'] > 0.1]
     column_rename_dict = {"LIQUID RATE(BLPD)": "L blpd", "OIL(BOPD)": "O bopd"}
