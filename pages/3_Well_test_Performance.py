cfce01cde4e2b3ce1f3df644e9d017d665b7701e
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-07-23 13:31:30 +0530

    Update 3_Well_test_Performance.py

--- pages/3_Well_test_Performance.py
+++ pages/3_Well_test_Performance.py
@@ -13,15 +13,6 @@ merged_data = pd.read_csv(data_url)
 # Ensure the 'TEST DATE' column is in datetime format
 merged_data['TEST DATE'] = pd.to_datetime(merged_data['TEST DATE'])
 
-# Find the latest date for each WELL STRING
-latest_dates = merged_data.groupby('WELL STRING')['TEST DATE'].max()
-
-# Join this info back onto the dataframe
-merged_data = merged_data.join(latest_dates, on='WELL STRING', rsuffix='_latest')
-
-# Only keep rows where TEST DATE is latest
-merged_data = merged_data[merged_data['TEST DATE'] == merged_data['TEST DATE_latest']]
-
 # Sidebar
 st.sidebar.title("Filters")
 
@@ -43,13 +34,16 @@ elif type_filter == 'GP':
     filtered_data = merged_data[(merged_data['Field'] == field_filter) & (merged_data['Type'] == type_filter) & 
                                  (merged_data['Flowing/Closed'] == flowing_closed_filter) & (merged_data['Free gas'] >= gp_filter)]
 
+# Find the latest date for each WELL STRING in the filtered data
+latest_dates = filtered_data.groupby('WELL STRING')['TEST DATE'].max()
+
+# Filter the WELL STRING in the filtered data based on the latest dates
+filtered_data = filtered_data[filtered_data.groupby('WELL STRING')['TEST DATE'].transform('max') == filtered_data['TEST DATE']]
+
 # Display the count of "WELL STRING" column values based on the filters
 st.title(f"Filtered WELL STRING values")
 st.write(filtered_data['WELL STRING'].unique())
 st.write(f"Total count: {filtered_data['WELL STRING'].nunique()}")
-
-# Filter original data to include only WELL STRING that appear in filtered_data
-merged_data = merged_data[merged_data['WELL STRING'].isin(filtered_data['WELL STRING'].unique())]
 
 # Interactive plots
 st.title(f"Interactive plots based on filters")
@@ -58,6 +52,7 @@ well_strings = filtered_data['WELL STRING'].unique()
 
 for well_string in well_strings:
     st.markdown(f"## {well_string}")
+    # Extract all recorded data for this WELL STRING from the original dataset
     well_data = merged_data[merged_data['WELL STRING'] == well_string]
     
     if type_filter == 'OP':
@@ -78,3 +73,4 @@ for well_string in well_strings:
         sns.lineplot(x='TEST DATE', y='Free gas', data=well_data, ax=ax)
         ax.set_title('Free gas over time')
         st.pyplot(fig)
+
