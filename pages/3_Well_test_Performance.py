64bf399f30591b0d8762f7661c75ccb361d59673
PrateekKumar2109 <101380064+PrateekKumar2109@users.noreply.github.com>
2023-07-23 13:27:49 +0530

    Update 3_Well_test_Performance.py

--- pages/3_Well_test_Performance.py
+++ pages/3_Well_test_Performance.py
@@ -13,6 +13,15 @@ merged_data = pd.read_csv(data_url)
 # Ensure the 'TEST DATE' column is in datetime format
 merged_data['TEST DATE'] = pd.to_datetime(merged_data['TEST DATE'])
 
+# Find the latest date for each WELL STRING
+latest_dates = merged_data.groupby('WELL STRING')['TEST DATE'].max()
+
+# Join this info back onto the dataframe
+merged_data = merged_data.join(latest_dates, on='WELL STRING', rsuffix='_latest')
+
+# Only keep rows where TEST DATE is latest
+merged_data = merged_data[merged_data['TEST DATE'] == merged_data['TEST DATE_latest']]
+
 # Sidebar
 st.sidebar.title("Filters")
 
@@ -35,8 +44,12 @@ elif type_filter == 'GP':
                                  (merged_data['Flowing/Closed'] == flowing_closed_filter) & (merged_data['Free gas'] >= gp_filter)]
 
 # Display the count of "WELL STRING" column values based on the filters
-st.title(f"Count of WELL STRING values based on filters")
-st.write(filtered_data['WELL STRING'].value_counts())
+st.title(f"Filtered WELL STRING values")
+st.write(filtered_data['WELL STRING'].unique())
+st.write(f"Total count: {filtered_data['WELL STRING'].nunique()}")
+
+# Filter original data to include only WELL STRING that appear in filtered_data
+merged_data = merged_data[merged_data['WELL STRING'].isin(filtered_data['WELL STRING'].unique())]
 
 # Interactive plots
 st.title(f"Interactive plots based on filters")
@@ -45,7 +58,7 @@ well_strings = filtered_data['WELL STRING'].unique()
 
 for well_string in well_strings:
     st.markdown(f"## {well_string}")
-    well_data = filtered_data[filtered_data['WELL STRING'] == well_string]
+    well_data = merged_data[merged_data['WELL STRING'] == well_string]
     
     if type_filter == 'OP':
         fig, axs = plt.subplots(2, figsize=(10, 10))
